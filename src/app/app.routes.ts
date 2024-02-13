bc1c3ede567f069d84c8faa6dd5bfc15d24f8f52
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-02-13 21:43:44 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'mainMap',
+    loadComponent: () => import('@maps/map-screen.component')
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'mainMap'
+  }
+];
---
+++ src/app/maps/map-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'map-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-screen.component.html',
+})
+export default class MapScreenComponent {
+
+}
